7a0cf46ba5ea648bf8e7a174ead9bdd7f72b4e55
mango2309 <sebastian.largo@udla.edu.ec>
2024-12-03 07:18:35 -0500

    Agregar archivos de proyecto.

---
+++ PruebaProgreso2SebastianLargo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PruebaProgreso2SebastianLargo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PruebaProgreso2SebastianLargo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaProgreso2SebastianLargo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
